c602672a9e7e461bd462c67b0acfade98483caec
manvirsingh <manvir.singh@nagarro.com>
2026-04-04 17:22:36 +0530

    Add routing with react-router-dom and implement main application structure

--- Blog-Assignment/blog-app/src/App.jsx
+++ Blog-Assignment/blog-app/src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Toast from './components/Toast'
+import HomePage from './pages/HomePage'
+import PostDetailsPage from './pages/PostDetailsPage'
+import PostFormPage from './pages/PostFormPage'
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-slate-100 text-slate-900">
+        <Toast />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/posts/new" element={<PostFormPage />} />
+          <Route path="/posts/:postId" element={<PostDetailsPage />} />
+          <Route path="/posts/:postId/edit" element={<PostFormPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- Blog-Assignment/blog-app/src/main.jsx
+++ Blog-Assignment/blog-app/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App.jsx'
+import { store } from './app/store'
+import { UiProvider } from './context/UiContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <UiProvider>
+        <App />
+      </UiProvider>
+    </Provider>
   </StrictMode>,
 )
